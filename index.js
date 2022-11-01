78ac3c67ca9861809a24cd865e557b7dbc6b39b4
Dereck Cervantes <dereckfer51@gmail.com>
2022-11-01 13:06:16 -0400

    commit tree d9d016f95c9fdd360c0339f6e6e946cde59edce4

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+function superbowlWin(winRecord){
+    const found = winRecord.find(({result}) => result === "W");
+    if(found){
+        return found.year;
+    }else 
+    return undefined;;
+}
